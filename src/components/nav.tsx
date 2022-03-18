12ac0c0b33d147eb3296744056f92ee4ca8c377e
Brad Cypert <brad.cypert@gmail.com>
2022-03-18 10:17:56 -0400

    add cli with bosun; style changes

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,14 +5,14 @@ interface Props {}
 
 const Nav: React.FunctionComponent<Props> = ({}) => {
     return (
-        <nav className="m-2 rounded border bg-green-500 border-green-500 dark:border-gray-800 dark:bg-gray-800">
+        <nav className="m-2 rounded border bg-green-200 border-green-200 dark:border-gray-800 dark:bg-gray-800">
             <ul className="flex flex-col md:flex-row gap-4 text-center">
-                <li className="p-4 flex items-center justify-center flex-1 md:border-r-2 border-green-700 dark:border-gray-400">
+                <li className="p-1 flex items-center justify-center flex-1 md:border-r-2 border-green-300 dark:border-gray-400">
                     <Link to="/">BradCypert.com</Link>
                 </li>
-                <li className="p-4 flex items-center justify-center flex-1 group md:border-r-2 border-green-700 dark:border-gray-400 cursor-pointer">
+                <li className="p-1 flex items-center justify-center flex-1 group md:border-r-2 border-green-300 dark:border-gray-400 cursor-pointer">
                     <div>Language Specific Resources</div>
-                    <ul className="absolute hidden hover:block group-hover:block border bg-green-500 border-green-700 dark:border-gray-800 dark:bg-gray-800 z-50 p-4 transform translate-y-2/4">
+                    <ul className="absolute hidden hover:block group-hover:block border bg-green-200 border-green-300 dark:border-gray-800 dark:bg-gray-800 z-50 p-1 transform translate-y-2/4">
                         <li>
                             <Link to="/clojure-resources">
                                 Clojure
@@ -50,12 +50,12 @@ const Nav: React.FunctionComponent<Props> = ({}) => {
                         </li>
                     </ul>
                 </li>
-                <li className="p-4 flex items-center justify-center flex-1 md:border-r-2 border-green-700 dark:border-gray-400">
+                <li className="p-1 flex items-center justify-center flex-1 md:border-r-2 border-green-300 dark:border-gray-400">
                     <Link to="/blog">Blog</Link>
                 </li>
-                <li className="p-4 flex items-center justify-center flex-1 group cursor-pointer">
+                <li className="p-1 flex items-center justify-center flex-1 group cursor-pointer">
                     <div>About</div>
-                    <ul className="absolute hidden hover:block group-hover:block border bg-green-500 border-green-700 dark:border-gray-800 dark:bg-gray-800 z-50 p-4 transform translate-y-2/4">
+                    <ul className="absolute hidden hover:block group-hover:block border bg-green-200 border-green-300 dark:border-gray-800 dark:bg-gray-800 z-50 p-1 transform translate-y-2/4">
                         <li>
                             <Link to="/contact">
                                 Contact
