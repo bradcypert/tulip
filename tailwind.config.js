ed2a1ba69f9a64140f38a1fe7318778f8005464e
Brad <brad.cypert@gmail.com>
2022-06-17 15:38:41 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-    purge: {
-        content: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
-        options: {
-            safelist: {
-                standard: [/^bg-/, /^border-/, /^h-36/, /^w-36/],
-            },
-            blocklist: [/^debug-/],
-            keyframes: false,
-            fontFace: true,
-          },
+    content: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+    options: {
+      safelist: {
+        standard: [/^bg-/, /^border-/, /^h-36/, /^w-36/],
+      },
+      blocklist: [/^debug-/],
+      keyframes: false,
+      fontFace: true,
     },
     theme: {},
     variants: {
